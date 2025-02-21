913c451cb7f310cf96def8325ec33cbfefa52e1b
simitgamdha72 <tatva.pcs72@outlook.com>
2025-02-21 18:13:57 +0530

    improve pagenation

--- ViewModels/UserListViewModel.cs
+++ ViewModels/UserListViewModel.cs
@@ -7,4 +7,5 @@ public class UserListViewModel
      public List<Role> Role { get; set; }
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
+   
 }
--- obj/Debug/net7.0/pizzashop.AssemblyInfo.cs
+++ obj/Debug/net7.0/pizzashop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pizzashop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8098aa9c9ff1a0143a0d5cbe6a142013774f940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e559812dc686a68ed78c9e783adece5c79b5ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("pizzashop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pizzashop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
