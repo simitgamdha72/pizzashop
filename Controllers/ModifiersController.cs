afd2488589e2e5718767880bf7281802a3445a5c
simitgamdha72 <tatva.pcs72@outlook.com>
2025-03-20 19:06:39 +0530

    MODIFIERS

--- Controllers/ModifiersController.cs
+++ Controllers/ModifiersController.cs
@@ -16,43 +16,13 @@ namespace pizzashop.Controllers
 
         public IActionResult Modifiers()
         {
-            var modifiers = _context.ModifiersGroups.ToList();
-
-            var viewModel = new ModifiersViewModel
-            {
-                modifiersGroups = modifiers,
-            };
-            return View(viewModel);
+            return View();
         }
 
-        [HttpGet]
-        public IActionResult addModifierModal()
+        public IActionResult GetModifiersGroup()
         {
-            return PartialView("_AddModifierModal");
-        }
-
-        // [HttpPost]
-        // public IActionResult addModifier(ModifiersViewModel model)
-        // {
-        //     var viewModel = new ModifiersGroup
-        //     {
-        //         Name = model.Name,
-        //         Description = model.Description,
-        //     };
-
-
-
-        //     _context.ModifiersGroups.Add(viewModel);
-        //     _context.SaveChanges();
-
-        //     return RedirectToAction("Modifiers", "Modifiers");
-
-
-        // }
-
-        public IActionResult GetModifiers()
-        {
-            var modifiers = _context.ModifiersGroups.ToList();
+            var modifiers = _context.ModifiersGroups.Where(x => x.Isdeleted != true)
+            .ToList();
 
             var viewModel = new ModifiersViewModel
             {
@@ -61,21 +31,225 @@ namespace pizzashop.Controllers
             return PartialView("_ModifierSidebar", viewModel);
         }
 
-        public IActionResult AddModifier(ModifiersViewModel model)
+        [HttpGet]
+        public IActionResult addModifierModalGroup()
         {
+            return PartialView("_AddModifierModalGroup");
+        }
+
+        public IActionResult AddModifierGroup(ModifiersViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["SomethingIsMissing"] = "Something Went Wrong";
+                return Json(false);
+            }
+
+            var validator = _context.ModifiersGroups.FirstOrDefault(x => x.Name == model.Name);
+            if (validator != null)
+            {
+                TempData["ModifiersGroupExist"] = "ModifiersGroup is Alreday Exist";
+                return Json(false);
+            }
+
+
             var viewModel = new ModifiersGroup
             {
                 Name = model.Name,
                 Description = model.Description,
             };
 
-
-
             _context.ModifiersGroups.Add(viewModel);
             _context.SaveChanges();
 
-
+            TempData["ModifiersGroupAdd"] = "New ModifiersGroup is Added ";
             return Json(new { success = true });
         }
+
+        public IActionResult EditModifierGroupModal(int id)
+        {
+            ModifiersGroup? modifiersGroup = _context.ModifiersGroups.FirstOrDefault(x => x.Id == id);
+
+            if (modifiersGroup == null)
+            {
+                TempData["SomethingIsMissing"] = "Something Went Wrong";
+                return RedirectToAction("Modifiers", "Midifiers");
+            }
+
+            // var modifiersItem = _context.MenuModifiers.Where(x => x.ModifierGroupId == id)
+            // .ToList();
+
+            // if (modifiersItem != null)
+            // {
+            //     var viewModel = new ModifiersViewModel
+            //     {
+            //         Id = modifiersGroup.Id,
+            //         Name = modifiersGroup.Name,
+            //         Description = modifiersGroup.Description,
+            //         menuModifiers = modifiersItem,
+            //     };
+            //     return PartialView("_EditModifierModal", viewModel);
+            // }
+            // else
+            // {
+            //     var viewModel = new ModifiersViewModel
+            //     {
+            //         Id = modifiersGroup.Id,
+            //         Name = modifiersGroup.Name,
+            //         Description = modifiersGroup.Description,
+            //         menuModifiers = modifiersItem,
+            //     };
+
+            var viewModel = new ModifiersViewModel
+            {
+                Id = modifiersGroup.Id,
+                Name = modifiersGroup.Name,
+                Description = modifiersGroup.Description,
+
+            };
+
+            return PartialView("_EditModifierGroupModal", viewModel);
+
+        }
+
+        [HttpPost]
+        public IActionResult EditModifierGroup(ModifiersViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["SomethingIsMissing"] = "Something Went Wrong";
+                return RedirectToAction("Modifiers", "Midifiers");
+            }
+
+            ModifiersGroup? modifiersGroup = _context.ModifiersGroups.FirstOrDefault(x => x.Id == model.Id);
+            var validator = _context.ModifiersGroups.FirstOrDefault(x => x.Name == model.Name);
+
+            if (validator != null)
+            {
+                if (modifiersGroup.Id != validator.Id)
+                {
+                    TempData["ModifiersGroupExist"] = "ModifiersGroup is Alreday Exist";
+                    return Json(false);
+                }
+            }
+
+            modifiersGroup.Name = model.Name;
+            modifiersGroup.Description = model.Description;
+
+            _context.ModifiersGroups.Update(modifiersGroup);
+            _context.SaveChanges();
+
+            TempData["ModifiersGroupEdit"] = "ModifiersGroup is Updated";
+            return Json(true);
+        }
+
+
+        public IActionResult DeleteModifierGroupModal(int id)
+        {
+            var modifiersGroup = _context.ModifiersGroups.FirstOrDefault(x => x.Id == id);
+            var viewModel = new ModifiersViewModel
+            {
+                Id = id,
+            };
+
+            return PartialView("_DeleteModifierGroupModal", viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteModifiersGroup(int id)
+        {
+            var modifiersgroup = _context.ModifiersGroups.FirstOrDefault(x => x.Id == id);
+            modifiersgroup.Isdeleted = true;
+            _context.ModifiersGroups.Update(modifiersgroup);
+            _context.SaveChanges();
+
+            TempData["ModifiersGroupIsDeleted"] = "ModifiersGroup Deleted Successfully";
+            return Json(true);
+        }
+
+        public IActionResult GetExistingModifiers(int id)
+        {
+            var ExistModifiers = _context.MenuModifiers.Where(x => x.ModifierGroupId == id).ToList();
+
+            if (ExistModifiers != null)
+            {
+                var viewModel = new ModifiersViewModel
+                {
+                    menuModifiers = ExistModifiers,
+                };
+                return PartialView("_ExistingModifiers", viewModel);
+
+            }
+            else
+            {
+                return Json(false);
+            }
+
+        }
+
+        public IActionResult GetModifiers(int id)
+        {
+            var ModifiersList = _context.MenuModifiers.Where(x => x.ModifierGroupId == id && x.Isdeleted != true).ToList();
+
+            var viewModel = new PaginatedModifiersViewModel
+            {
+                Modifiers = ModifiersList,
+            };
+
+            return PartialView("_ModifiersList", viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult addModifierModal()
+        {
+            var modifiersGroup = _context.ModifiersGroups.ToList();
+            var units = _context.Units.ToList();
+
+            var model = new MenuModifiersViewModel
+            {
+                modifiersGroups = modifiersGroup,
+                units = units,
+            };
+            return PartialView("_AddModifierModal", model);
+        }
+
+        public IActionResult AddModifier(MenuModifiersViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["SomethingIsMissing"] = "Something Went Wrong";
+                return Json(false);
+            }
+
+            var validator = _context.MenuModifiers.FirstOrDefault(x => x.Name == model.Name && x.ModifierGroupId == model.ModifierGroupId);
+            if (validator != null)
+            {
+                TempData["ModifierExist"] = "Modifier is Alreday Exist";
+                return Json(false);
+            }
+
+
+            var viewModel = new MenuModifier
+            {
+                ModifierGroupId = model.ModifierGroupId,
+                Name = model.Name,
+                Rate = model.Rate,
+                Quantity = model.Quantity,
+                UnitId = model.UnitId,
+                Description = model.Description,
+            };
+
+            _context.MenuModifiers.Add(viewModel);
+            _context.SaveChanges();
+
+            TempData["ModifierAdd"] = "New Modifier is Added ";
+            return Json(new { success = true });
+        }
+
+
+
+
+
+
     }
 }
--- Controllers/RoleandPermissionController.cs
+++ Controllers/RoleandPermissionController.cs
@@ -1,22 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using pizzashop.Models.Models;
 using pizzashop.Models.ViewModels;
 
 namespace pizzashop.Controllers;
 
 public class RoleandPermissionController : Controller
 {
-
-    private readonly PizzashopContext _context;
     private readonly iroleandpermissionservice _roleandpermissionservice;
 
-
-    public RoleandPermissionController(PizzashopContext context, iroleandpermissionservice roleandpermissionservice)
+    public RoleandPermissionController(iroleandpermissionservice roleandpermissionservice)
     {
-        _context = context;
         _roleandpermissionservice = roleandpermissionservice;
-
     }
 
     [HttpGet]
@@ -29,7 +22,6 @@ public class RoleandPermissionController : Controller
     public IActionResult permission(int id)
     {
         var model = _roleandpermissionservice.GetPermission(id);
-
         return View(model);
     }
 
@@ -37,37 +29,7 @@ public class RoleandPermissionController : Controller
     [ValidateAntiForgeryToken]
     public IActionResult SavePermissions(RolePermissionsViewModel model)
     {
-        // foreach (var perm in model.Permissions)
-        // {
-        //     var rolewisePermission = await _context.Roleswisepermissions
-        //         .FirstOrDefaultAsync(rp => rp.RoleId == model.RoleId && rp.PermissionId == perm.PermissionId);
-
-        //     if (rolewisePermission == null)
-        //     {
-        //         // Add new permission if not found
-        //         _context.Roleswisepermissions.Add(new Roleswisepermission
-        //         {
-        //             RoleId = model.RoleId,
-        //             PermissionId = perm.PermissionId,
-        //             CanView = perm.CanView,
-        //             CanAddEdit = perm.CanAddEdit,
-        //             CanDelete = perm.CanDelete
-        //         });
-        //     }
-        //     else
-        //     {
-        //         // Update existing permissions
-        //         rolewisePermission.CanView = perm.CanView;
-        //         rolewisePermission.CanAddEdit = perm.CanAddEdit;
-        //         rolewisePermission.CanDelete = perm.CanDelete;
-        //     }
-        // }
-
-        // await _context.SaveChangesAsync();
-
         _roleandpermissionservice.SavePermissions(model);
-        return RedirectToAction("Userlist", "Account"); // Or redirect wherever appropriate
-
+        return RedirectToAction("Userlist", "Account");
     }
-
 }
